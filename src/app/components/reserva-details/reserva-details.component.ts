a45eb060f14e93fbf7ea6a180b33b347b08d09df
Victoria Acuna <victoria.acuna@correo.unimet.edu.ve>
2019-11-02 09:23:18 -0400

    admin

--- src/app/components/reserva-details/reserva-details.component.ts
+++ src/app/components/reserva-details/reserva-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Order } from 'src/app/models/orders';
 
 @Component({
   selector: 'app-reserva-details',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservaDetailsComponent implements OnInit {
 
+  @Input() order: Order;
   constructor() { }
 
   ngOnInit() {
